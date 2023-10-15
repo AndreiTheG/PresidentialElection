03243240607087075c1c4e2d8a8fbaf1b80e9e01
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-10-15 13:47:02 +0300

    ChangeTheVariableNamesForIds

--- PresidentialElection/src/main/java/com/example/PresidentialElection/Controller/CandidateController.java
+++ PresidentialElection/src/main/java/com/example/PresidentialElection/Controller/CandidateController.java
@@ -60,14 +60,14 @@ public class CandidateController {
     // Save the values of the id of user and the id of the current applicant so we can
     // display the username of the user and the details of the applicant when the method will
     // redirect to "applicant/:{idCandidate}/candidate-page-profile"
-    @GetMapping("visits-candidate-profile/:{candidateId}")
-    public String getAccessToCandidateProfilePage(/*@PathVariable("userId") long userId,*/ @PathVariable("candidateId") long candidateId) {
-        //this.userId = userId;
-        if (this.userId == 0) {
-            return "redirect:/user/login-or-register";
-        }
-        return "redirect:/candidate/:" + candidateId + "/candidate-page-profile";
-    }
+//    @GetMapping("visits-candidate-profile/:{candidateId}")
+//    public String getAccessToCandidateProfilePage(/*@PathVariable("userId") long userId,*/ @PathVariable("candidateId") long candidateId) {
+//        //this.userId = userId;
+//        if (this.userId == 0) {
+//            return "redirect:/user/login-or-register";
+//        }
+//        return "redirect:/candidate/:" + candidateId + "/candidate-page-profile";
+//    }
 
     // Display the profile page of the candidate with the id equal to the value of idCandidate
     @GetMapping(":{candidateId}/candidate-page-profile")
